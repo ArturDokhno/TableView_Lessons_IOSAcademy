d5ffa751daae01ef3e58ad6fc0cbcaf22f9967fe
ArturDokhno <artur.dohno@icloud.com>
2022-06-05 19:29:23 +0500

    Кастомная ячейка с тремя разными видами внутри

--- TableView_Lessons_IOSAcademy/CustomCells/CustomCellsViewController.swift
+++ TableView_Lessons_IOSAcademy/CustomCells/CustomCellsViewController.swift
@@ -8,22 +8,65 @@
 import UIKit
 
 class CustomCellsViewController: UIViewController {
+    
+    @IBOutlet var tableView: UITableView!
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        tableView.register(FieldTableViewCell.nib(), forCellReuseIdentifier: FieldTableViewCell.identifier)
+        tableView.register(MyTableViewCell.nib(), forCellReuseIdentifier: MyTableViewCell.identifier)
+        tableView.delegate = self
+        tableView.dataSource = self
+    }
+
+}
+
+extension CustomCellsViewController: UITableViewDelegate {
+    
+}
+
+extension CustomCellsViewController: UITableViewDataSource {
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        10
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        if indexPath.row > 5 {
+            let fieldCell = tableView.dequeueReusableCell(withIdentifier: FieldTableViewCell.identifier, for: indexPath) as! FieldTableViewCell
+            
+            return fieldCell
+        }
+        
+        if indexPath.row > 3 {
+            let customCell = tableView.dequeueReusableCell(withIdentifier: MyTableViewCell.identifier, for: indexPath) as! MyTableViewCell
+            customCell.configure(with: "Custom Cell", imageName: "gear")
+            return customCell
+        }
+        
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        
+        cell.textLabel?.text = "Hello World"
+        
+        let mySwitch = UISwitch()
+        mySwitch.addTarget(self, action: #selector(didChangeSwitch(_:)), for: .valueChanged)
+        mySwitch.isOn = true
+        cell.accessoryView = mySwitch
+        
+        return cell
     }
-    */
-
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        70
+    }
+    
+    @objc func didChangeSwitch(_ sender: UISwitch) {
+        if sender.isOn {
+            print("User turned it on")
+        } else {
+            print("It's now off")
+        }
+    }
 }
