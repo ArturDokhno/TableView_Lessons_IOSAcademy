fdb50e1502b57ecb061ddd4f07e4da954fb0f386
ArturDokhno <artur.dohno@icloud.com>
2022-06-05 16:41:59 +0500

    Initial Commit

---
+++ TableView_Lessons_IOSAcademy/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TableView_Lessons_IOSAcademy
+//
+//  Created by Артур Дохно on 05.06.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
